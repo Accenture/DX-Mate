b23431c5dfdd3dcb29792fdeb525728e1b4fb8ac
SRS_ACN_DX <stian.schikora@accenture.com>
2022-09-30 20:18:24 +0200

    Connecting push and pull cmds

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {openScratchOrg, sourcePushMetadata, createScratchOrg, importDummyData, deployUnpackagable, assignPermsets, generateLoginLink} from './commands';
+import {openScratchOrg, sourcePushMetadata, createScratchOrg, importDummyData, deployUnpackagable, assignPermsets, generateLoginLink, sourcePullMetadata} from './commands';
 import { installDependencies, addPackageKey, addDependency } from './packageCommands';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,6 +25,14 @@ function registerOrgCommands(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('dxmate.addPackageKey', () => {
 		addPackageKey();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('dxmate.pullSource', () => {
+		sourcePullMetadata();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('dxmate.pushSource', () => {
+		sourcePushMetadata();
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('dxmate.importDummyData', () => {
