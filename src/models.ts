5d0373786d2166b166572d0afc1903b1af6329d0
SRS_ACN_DX <stian.schikora@accenture.com>
2022-11-24 21:31:13 +0100

    Scratch org interface

--- src/models.ts
+++ src/models.ts
@@ -11,6 +11,24 @@ export class PackageDirectory {
     dependencies: Dependency[] = []; //Object with properties "package", and "versionNumber"
 }
 
+export default interface ScratchOrg {
+    failureMessage?: string;
+    tag?: string;
+    recordId?: string;
+    orgId?: string;
+    loginURL?: string;
+    signupEmail?: string;
+    username?: string;
+    alias?: string;
+    password?: string;
+    isScriptExecuted?: boolean;
+    expiryDate?: string;
+    accessToken?: string;
+    instanceURL?: string;
+    status?: string;
+    sfdxAuthUrl?: string;
+}
+
 export class Dependency {
     package: string = '';
     versionNumber: string = '';
