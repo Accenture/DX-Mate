fa095360d1c9b813fc4dd007a5cb28bf7ff606b3
SRS_ACN_DX <stian.schikora@accenture.com>
2022-11-24 14:15:46 +0100

    Hooked export command

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {openScratchOrg, sourcePushMetadata, importDummyData, generateLoginLink, sourcePullMetadata, createProject, assignDefaultPermsets, createScratchOrgJob, sourcePushMetadataJob, deployUnpackagableJob, openScratchOrgJob, importDummyDataJob, assignPermsetsJob} from './commands';
+import {openScratchOrg, sourcePushMetadata, importDummyData, generateLoginLink, sourcePullMetadata, createProject, assignDefaultPermsets, createScratchOrgJob, sourcePushMetadataJob, deployUnpackagableJob, openScratchOrgJob, importDummyDataJob, assignPermsetsJob, sfdxExportData} from './commands';
 import { EXTENSION_CONTEXT, PackageDirectory } from './models';
 import {inputUpdateDependencyKey, addDependency} from './dependencyCommands';
 import {getPackageDirectories, getPackageDirectoryInput} from './workspace';
@@ -75,6 +75,10 @@ function registerOrgCommands(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('dxmate.importDummyData', () => {
 		importDummyData();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('dxmate.exportData', () => {
+		sfdxExportData();
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('dxmate.assignPermissionsets', () => {
